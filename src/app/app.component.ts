b849c6f5ebb4d779226e1e57273a15eb6bbc0c66
Sten P <parv.sten@gmail.com>
2024-01-10 18:33:03 +0200

    stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    ReactiveFormsModule, 
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-project';
+
+  registerForm = this.fb.group({
+    id: "2",
+    domainName: "",
+    domain: "",
+  });
+
+  domainTypes = [
+    { id:1 , title: "ee"},
+    { id:2 , title: "eu"}
+  ]
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    console.log('submitted form ', this.registerForm.value)
+  }
 }
---
+++ src/app/enter-data/enter-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enter-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './enter-data.component.html',
+  styleUrl: './enter-data.component.css'
+})
+export class EnterDataComponent {
+
+}
